24f6635941191466d9b1b616a247d8a825917e90
kiranraj09 <kiranraj@adaptivebizapp.in>
2026-01-19 10:55:46 +0530

    uom page code changes

--- src/test/java/com/Sales/SalesInvoiceVoidTest.java
+++ src/test/java/com/Sales/SalesInvoiceVoidTest.java
@@ -575,20 +575,20 @@ public class SalesInvoiceVoidTest extends BaseClass {
 	public void ProductPage() throws InterruptedException {
 
 		ProductList.addAll(ProductSet);
-		
+
 		driver.navigate().to(url + "SalesPurchases/Product");
 		Thread.sleep(4000);
 		System.out.println("*Product Details Page*");
 		driver.manage().timeouts().pageLoadTimeout(200, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-	//	WebDriverWait wait = new WebDriverWait(driver, 60);
+		//	WebDriverWait wait = new WebDriverWait(driver, 60);
 		Product prod = new Product(driver);
-		
+
 		int productSetSize = ProductSet.size();
 		System.out.println("Product Set Size: " + productSetSize);
 		for (String product : ProductSet) {
-			
+
 			WebElement productcode = driver.findElement(By.xpath("(//input[@id='SearchString'])[1]"));
 			Thread.sleep(1000);
 			productcode.sendKeys(Keys.CONTROL + "a" + Keys.DELETE);
@@ -803,8 +803,8 @@ public class SalesInvoiceVoidTest extends BaseClass {
 
 		for (String uom : UOMSet) {
 
-			WebElement selectUom = driver.findElement(By.id("select2-DropDown-container"));
-			js.executeScript("arguments[0].click();", selectUom);
+			WebElement selectUom = driver.findElement(By.xpath("//span[@id='select2-DropDown-container']"));
+			selectUom.click();
 			WebElement UOMSearchField = driver.findElement(
 					By.xpath("//span[@id='select2-DropDown-container']//following::input[@type='search']"));
 			UOMSearchField.sendKeys(uom + Keys.ENTER);
@@ -1731,6 +1731,7 @@ public class SalesInvoiceVoidTest extends BaseClass {
 
 	}
 
+	//@Ignore
 	@Test(priority = 40, dependsOnMethods = "ERPLoginPage")
 	private void close() throws InterruptedException {
 		driver.quit();

c996b30d7422929d1258b6c46cfb40df689c42ac
kiranraj09 <kiranraj@adaptivebizapp.in>
2026-02-02 12:51:19 +0530

    purchase invoice delete code changes

--- src/test/java/com/Purchase/PurchaseGRNTest.java
+++ src/test/java/com/Purchase/PurchaseGRNTest.java
@@ -1684,7 +1684,8 @@ public class PurchaseGRNTest extends BaseClass {
 		Thread.sleep(3000);
 		WebElement delete = driver.findElement(By.xpath
 				("(//table[@id='purchasetable']//tbody//tr//td[normalize-space()='"+formatedTimestamp+"']//following::a[@title='Delete'])[1]"));
-		delete.click();
+		wait.until(ExpectedConditions.elementToBeClickable(delete));
+		js.executeScript("arguments[0].click();", delete);
 		Thread.sleep(2000);
 		click(pi.Delete);
 		Thread.sleep(1000);
